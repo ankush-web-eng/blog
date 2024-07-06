652576fd623aa4fad3da737c21ddcdfff162df1b
Ankush <deshwalankush23@gmail.com>
2024-07-06 19:46:29 +0530

    URL bug fixed

--- app/page.tsx
+++ app/page.tsx
@@ -13,11 +13,11 @@ interface form {
 
 export default async function Page() {
 
-  const res = await axios.get(`https://api.ankushsingh.tech`);
+  const res = await axios.get(`${process.env.GHOST_API_URL}`);
 
   return (
-    <div className="h-screen w-screen flex justify-center items-center px-4 py-6">
-      <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
+    <div className="md:h-screen flex justify-center items-center px-2 py-6">
+      <div className="grid grid-cols-1 gap-6 py-6 max-md:overflow-auto md:grid-cols-2 lg:grid-cols-3">
         {res.data.message.data !== null ? (
           res.data.message.data.map((ghost : form, index : any) => (
             <Ghost key={index} params={ghost} />
